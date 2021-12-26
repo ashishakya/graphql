bbaff52076d08e3475b3cb57a3e4337d667ab911
Ashish Shakya <ashishakya@outlook.com>
2021-12-26 10:21:55 +0545

    working with fake db

--- server.js
+++ server.js
@@ -9,20 +9,29 @@ const users = require('./users.json')
 
 const app = express();
 
-let fakeDB = [];
+let fakeDB = [
+    {id:1, name:'office', rent:25},
+    {id:2, name:'school', rent:35},
+];
 
 const schema = buildSchema(`
      type Person{
         name:String,
         email:String
      }
+     type Space {
+        name:String,
+        rent: Int
+     }
+     type Mutation {
+        addMessage(message:String): String,
+        createSpace(name: String, rent:Int): Space
+     }
      type Query {
         users: [Person],
         user(id:Int): Person,
-        getMessage: [String]     
-     }
-     type Mutation {
-        addMessage(message:String): String
+        getMessage: [String]  
+        getSpace(id:Int!):Space!   
      }
 `);
 
@@ -30,7 +39,9 @@ const root = {
     users: () => users,
     user: ({id}) => users.find(user=>user.id===id),
     addMessage: ({message}) => fakeDB.push(message),
-    getMessage: ()=>fakeDB
+    getMessage: ()=>fakeDB,
+    createSpace:({name, rent})=>fakeDB[fakeDB.length]=({id:fakeDB.length+1, name, rent}),
+    getSpace:({id})=>fakeDB.find(space => space.id === id),
 };
 
 app.use('/graphql', graphqlHTTP({
