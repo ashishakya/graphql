3ec661eb31a4468062ce196d5a97f24359d91d67
Ashish Shakya <ashishakya@outlook.com>
2021-12-28 11:18:07 +0545

    resolve interface type

--- resolvers.js
+++ resolvers.js
@@ -1,8 +1,21 @@
 const characters = require("./harrypotter.json");
 const resolvers = {
+    Character: {
+        __resolveType(character, context, info) {
+            console.log(info);
+            if (!!character.species) {
+                return "NonHuman" // is must be exact same as the type name
+            }
+            if (!character.species) {
+                return "Human";
+            }
+            return null;
+        }
+    },
     Query: {
-        human: () => characters.filter(character=>!character.species),
-        nonHuman: () => characters.filter(character=>!!character.species),
+        human: () => characters.filter(character => !character.species),
+        nonHuman: () => characters.filter(character => !!character.species),
+        characters: () => characters,
     },
 };
 
--- types.js
+++ types.js
@@ -38,6 +38,7 @@ const types = gql`
     type Query {
         human: [Human!]!
         nonHuman: [NonHuman!]!
+        characters: [Character!]!
     }
 `;
 
